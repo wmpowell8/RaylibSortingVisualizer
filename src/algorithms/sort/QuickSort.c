4de179fb9e6d8113fc071d1bc0f2293f7df0339f
William Powell "wmpowell8 <88069370+wmpowell8@users.noreply.github.com>
2022-12-15 18:50:05 -0800

    Fixed quicksort returning false Fixed quicksort returning false due to attempted out-of-bounds read which occurs when the part of the array currently being operated on begins with the very first element and the pivot is the least-valued element currently being operated on.

--- src/algorithms/sort/QuickSort.c
+++ src/algorithms/sort/QuickSort.c
@@ -2,14 +2,14 @@
 
 static bool _qs(Array array, size_t start, size_t end)
 {
-    if (end <= start)
+    if (end + 1 <= start + 1) // adding 1 first because of edge case when start == 0 and end == ULONG_MAX
         return true;
 
     Array_Result pivot = Array_at(array, start);
     Array_propagate_err(pivot);
     size_t ptr1 = start;
     size_t ptr2 = end;
-    while (ptr1 <= ptr2)
+    while (ptr1 + 1 <= ptr2 + 1) // adding 1 first because of edge case when ptr1 == 0
     {
         Array_Result value1 = Array_at(array, ptr1);
         Array_propagate_err(value1);
