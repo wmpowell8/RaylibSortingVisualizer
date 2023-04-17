bfef24c7915831e36ea7630e93dc849da04ef28e
William Powell "wmpowell8 <88069370+wmpowell8@users.noreply.github.com>
2023-04-16 22:04:58 -0700

    Fixed quick sort When there were multiple elements in an array that are the same, the algorithm would get caught in an infinite loop. This no longer happens, due to the pivot now being handled first.

--- src/algorithms/sort/QuickSort.c
+++ src/algorithms/sort/QuickSort.c
@@ -7,14 +7,16 @@ static bool _qs(Array array, size_t start, size_t end)
 
     Array_Result pivot = Array_at(array, start);
     Array_propagate_err(pivot);
-    size_t ptr1 = start;
+    size_t ptr1 = start + 1; // pivot will remain at the beginning until it is put in place after this partition
     size_t ptr2 = end;
+    bool everything_is_same = true;
     while (ptr1 + 1 <= ptr2 + 1) // adding 1 first because of edge case when ptr1 == 0
     {
         Array_Result value1 = Array_at(array, ptr1);
         Array_propagate_err(value1);
-        if (value1.value >= pivot.value)
+        if (value1.value > pivot.value)
         {
+            everything_is_same = false;
             Array_Result value2 = Array_at(array, ptr2);
             Array_propagate_err(value2);
             if (Array_set(array, ptr1, value2.value) == ARRAY_ERR)
@@ -25,10 +27,15 @@ static bool _qs(Array array, size_t start, size_t end)
         }
         else
         {
+            if (value1.value != pivot.value)
+                everything_is_same = false;
             ptr1++;
         }
     }
-    if (!_qs(array, start, ptr2))
+    if (everything_is_same)
+        return true; // no need to waste time partitioning off only the pivot
+    Array_swap(array, start, ptr2); // put the pivot in place
+    if (!_qs(array, start, ptr2 - 1))
         return false;
     if (!_qs(array, ptr1, end))
         return false;
